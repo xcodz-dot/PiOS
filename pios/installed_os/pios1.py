1c52fed88f847f6e1cb4caceb7db7a88adc589a2
xcodz-dot <amaanahmadkhan1980@gmail.com>
2020-10-27 18:05:26 +0530

    Major Updates and bug fixes

--- pios/installed_os/pios1.py
+++ pios/installed_os/pios1.py
@@ -1,4 +1,4 @@
-import denver
+import denverapi
 import os
 import json
 import secrets
@@ -14,24 +14,24 @@ import glob
 
 root = ''
 
-es = denver.ctext.ColoredText.escape
+es = denverapi.ctext.ColoredText.escape
 
-print = denver.ctext.print
-input = denver.ctext.input
+print = denverapi.ctext.print
+input = denverapi.ctext.input
 
 original_stdout = sys.stdout
 original_stderr = sys.stderr
 original_stdin = sys.stdin
 
-with open( "system/_installer.u256", "r+b" ) as file_i_sha:
+with open("system/_installer.u256", "r+b") as file_i_sha:
     sha256_installer = file_i_sha.read()
 
 
-class PiosShutdown( Exception ):
+class PiosShutdown(Exception):
     pass
 
 
-class PiosReboot( Exception ):
+class PiosReboot(Exception):
     pass
 
 
@@ -47,28 +47,28 @@ class AutoUi:
             sub_dirs = []
             actions = []
             for k, v in current_dir.items():
-                if isinstance( v, dict ):
-                    sub_dirs.append( k )
-                elif isinstance( v, str ):
-                    actions.append( k )
+                if isinstance(v, dict):
+                    sub_dirs.append(k)
+                elif isinstance(v, str):
+                    actions.append(k)
             sub_dirs.sort()
             actions.sort()
-            if len( self.ui_path ) > 0:
-                print( ", .." )
+            if len(self.ui_path) > 0:
+                print(", ..")
             index = 0
-            for index, name in zip( range( len( sub_dirs ) ), sub_dirs ):
-                print( ",{} {}".format( index, name ) )
+            for index, name in zip(range(len(sub_dirs)), sub_dirs):
+                print(",{} {}".format(index, name))
             last_index = index + 1
-            for index, name in zip( range( last_index, len( actions ) + last_index + 1 ), actions ):
-                print( ".{} {}".format( index, name ) )
-            user_input = input( "->", fore="magenta" )
-            if user_input == ".." and len( self.ui_path ) > 0:
-                self.ui_path.pop( -1 )
+            for index, name in zip(range(last_index, len(actions) + last_index + 1), actions):
+                print(".{} {}".format(index, name))
+            user_input = input("->", fore="magenta")
+            if user_input == ".." and len(self.ui_path) > 0:
+                self.ui_path.pop(-1)
             elif user_input.isnumeric():
-                if int( user_input ) in range( 0, last_index ):
-                    self.ui_path.append( sub_dirs[int( user_input )] )
-                elif int( user_input ) in range( last_index, index + 1 ):
-                    self.command( current_dir[actions[last_index - int( user_input )]] )
+                if int(user_input) in range(0, last_index):
+                    self.ui_path.append(sub_dirs[int(user_input)])
+                elif int(user_input) in range(last_index, index + 1):
+                    self.command(current_dir[actions[last_index - int(user_input)]])
             clear()
 
     def get(self):
@@ -91,10 +91,10 @@ def install_app(file_name):
 
 
 def redirect_io_to_socket(s: socket.socket):
-    file = s.makefile( "w", buffering=None )
+    file = s.makefile("w", buffering=None)
     sys.stdout = file
     sys.stderr = file
-    sys.stdin = s.makefile( "r", buffering=None )
+    sys.stdin = s.makefile("r", buffering=None)
 
 
 def redirect_io_to_console():
@@ -104,14 +104,14 @@ def redirect_io_to_console():
 
 
 def make_directory_if_not_present(directory):
-    if not os.path.isdir( directory ):
-        os.mkdir( directory )
+    if not os.path.isdir(directory):
+        os.mkdir(directory)
 
 
 def make_file_if_not_present(file, data):
-    if not os.path.isfile( file ):
-        with open( file, "w" if isinstance( data, str ) else "w+b" ) as fp:
-            fp.write( data )
+    if not os.path.isfile(file):
+        with open(file, "w" if isinstance(data, str) else "w+b") as fp:
+            fp.write(data)
 
 
 def generate_default_directories_and_files():
@@ -120,7 +120,7 @@ def generate_default_directories_and_files():
             "enabled": False,
             "host-ipv4": "127.0.0.1",
             "host-port": 3700,
-            "join-password": base64.encodebytes( secrets.token_bytes( 4 ) ).decode()
+            "join-password": base64.encodebytes(secrets.token_bytes(4)).decode()
         },
         "security": {
             "installation": {
@@ -136,39 +136,39 @@ def generate_default_directories_and_files():
                 "user-interface": "auto-ui",
             }
     }
-    make_directory_if_not_present( "home" )
-    make_directory_if_not_present( "user" )
-    make_directory_if_not_present( "user/programs" )
-    make_directory_if_not_present( "system" )
-    make_directory_if_not_present( "user/bin" )
-    if not os.path.isfile( "system/settings.json" ):
-        with open( "system/settings.json", "w" ) as file:
-            json.dump( default_settings, file, indent=4, sort_keys=True )
+    make_directory_if_not_present("home")
+    make_directory_if_not_present("user")
+    make_directory_if_not_present("user/programs")
+    make_directory_if_not_present("system")
+    make_directory_if_not_present("user/bin")
+    if not os.path.isfile("system/settings.json"):
+        with open("system/settings.json", "w") as file:
+            json.dump(default_settings, file, indent=4, sort_keys=True)
 
 
 def read_file_str(file_name):
-    with open( file_name ) as file:
+    with open(file_name) as file:
         return file.read()
 
 
 def read_file_bytes(file_name):
-    with open( file_name, "w+b" ) as file:
+    with open(file_name, "w+b") as file:
         return file.read()
 
 
 def write_file(file, data):
-    with open( file, "w" if isinstance( data, str ) else "w+b" ) as fp:
-        fp.write( data )
+    with open(file, "w" if isinstance(data, str) else "w+b") as fp:
+        fp.write(data)
 
 
 def discover_modules(path):
     modules = []
     for directory in path:
-        if os.path.isdir( directory ):
-            modules.extend( glob.glob( f"{directory}/*.py" ) )
-            modules.extend( glob.glob( f"{directory}/*.pyc" ) )
-            modules.extend( glob.glob( f"{directory}/*.pyw" ) )
-            modules.extend( glob.glob( f"{directory}/*.pyo" ) )
+        if os.path.isdir(directory):
+            modules.extend(glob.glob(f"{directory}/*.py"))
+            modules.extend(glob.glob(f"{directory}/*.pyc"))
+            modules.extend(glob.glob(f"{directory}/*.pyw"))
+            modules.extend(glob.glob(f"{directory}/*.pyo"))
 
 
 def start_ui(ui_type):
@@ -182,58 +182,58 @@ def start_ui(ui_type):
             "Terminal": "start_ui('terminal')"
         }
         clear()
-        ui = AutoUi( ui, execute_command )
+        ui = AutoUi(ui, execute_command)
         ui.start_interface()
     elif ui_type == "terminal":
-        env = os.listdir( f"{root}/system/env/" )
-        environment_variables = {k: read_file_str( f"{root}/system/env/{k}" ) for k in env}
-        pi_path = environment_variables["PATH"].split( ';' )
-        os.chdir( root )
+        env = os.listdir(f"{root}/system/env/")
+        environment_variables = {k: read_file_str(f"{root}/system/env/{k}") for k in env}
+        pi_path = environment_variables["PATH"].split(';')
+        os.chdir(root)
 
         clear()
         # noinspection PyCallByClass
-        print( denver.ctext.ColoredText.escape( "Copyright (c) 2020 xcodz." ) )
+        print(denverapi.ctext.ColoredText.escape("Copyright (c) 2020 xcodz."))
         # noinspection PyCallByClass
-        print( denver.ctext.ColoredText.escape( f"{{fore_blue}}PiTerminal {{fore_green}}[{terminal_version}]" ) )
+        print(denverapi.ctext.ColoredText.escape(f"{{fore_blue}}PiTerminal {{fore_green}}[{terminal_version}]"))
         terminal_running = True
         while terminal_running:
-            user_command = input( es( os.getcwd().replace( os.sep, "/" ) + "{fore_magenta}$ " ) )
-            command = shlex.split( user_command, True )
+            user_command = input(es(os.getcwd().replace(os.sep, "/") + "{fore_magenta}$ "))
+            command = shlex.split(user_command, True)
             new_command = []
             for x in command:
-                s = list( x )
-                while match := re.match( r"(\${(\w*?)})", ''.join( s ) ):  # Regex Expression to match this syntax
-                    variable_value = match.group( 1 )  # ${VARIABLE_NAME}
+                s = list(x)
+                while match := re.match(r"(\${(\w*?)})", ''.join(s)):  # Regex Expression to match this syntax
+                    variable_value = match.group(1)  # ${VARIABLE_NAME}
                     try:
-                        variable_value = environment_variables[match.group( 2 )]
+                        variable_value = environment_variables[match.group(2)]
                     finally:
                         s[match.start(): match.end()] = variable_value
-                new_command.append( ''.join( s ) )
+                new_command.append(''.join(s))
             # noinspection PyBroadException
             try:
-                if len( new_command ) == 0:
+                if len(new_command) == 0:
                     continue
                 elif new_command[0] == "exit":
                     terminal_running = False
                 elif new_command[0] == "setenv":
-                    with open( f"{root}/system/env/{new_command[1]}" ) as file:
-                        file.write( new_command[2] )
+                    with open(f"{root}/system/env/{new_command[1]}") as file:
+                        file.write(new_command[2])
                 elif new_command[0] == "refresh":
-                    env = os.listdir( f"{root}/system/env/" )
-                    environment_variables = {k: read_file_str( f"{root}/system/env/{k}" ) for k in env}
-                    pi_path = environment_variables["PATH"].split( ";" )
+                    env = os.listdir(f"{root}/system/env/")
+                    environment_variables = {k: read_file_str(f"{root}/system/env/{k}") for k in env}
+                    pi_path = environment_variables["PATH"].split(";")
                 elif new_command[0] == "cd":
-                    os.chdir( new_command[1] )
+                    os.chdir(new_command[1])
                 elif new_command[0] == "delenv":
-                    os.remove( f"{root}/system/env/{new_command[1]}" )
+                    os.remove(f"{root}/system/env/{new_command[1]}")
                 elif new_command[0] == "echo":
-                    print( new_command[1] )
+                    print(new_command[1])
                 elif new_command[0] == "pecho":
-                    print( *new_command[1].split( ";" ), sep="\n" )
+                    print(*new_command[1].split(";"), sep="\n")
                 elif new_command[0] == "dlp":
                     print()
                 elif new_command[0] == "help":
-                    print( """
+                    print("""
 Builtin Commands:
     setenv name value                Set an environment variable
     help                             See this help message
@@ -243,33 +243,33 @@ Builtin Commands:
     delenv name                      Delete an environment variable
     echo value                       Print a value
     pecho value                      Print a value split at ';' 
-    dlp url                          Download a package and install""" )
+    dlp url                          Download a package and install""")
             except:
                 error_msg = traceback.format_exc()
-                print( error_msg )
+                print(error_msg)
             finally:
                 print()
 
 
 def execute_script(arguments: list):
-    return subprocess.run( arguments, stderr=sys.stderr, stdout=sys.stdout, stdin=sys.stdin )
+    return subprocess.run(arguments, stderr=sys.stderr, stdout=sys.stdout, stdin=sys.stdin)
 
 
 def execute_command(statement):
-    exec( statement, globals() )
+    exec(statement, globals())
 
 
 def start_os():
     clear()
-    print( "Booting PiOS", fore="green" )
+    print("Booting PiOS", fore="green")
     generate_default_directories_and_files()
-    print( "Welcome" )
-    sleep( 1 )
+    print("Welcome")
+    sleep(1)
     clear()
 
-    settings = json.loads( read_file_str( "system/settings.json" ) )
+    settings = json.loads(read_file_str("system/settings.json"))
 
-    start_ui( settings["system"]["user-interface"] )
+    start_ui(settings["system"]["user-interface"])
 
 
 def main():
@@ -282,18 +282,18 @@ def main():
             pios_running = False
         except PiosShutdown:
             clear()
-            print( "Shutting Down", fore="red" )
+            print("Shutting Down", fore="red")
             pios_running = False
         except PiosReboot:
             clear()
-            print( "Rebooting", fore="red" )
+            print("Rebooting", fore="red")
         except KeyboardInterrupt:
             clear()
-            print( "Forced Rebooting", fore="red" )
+            print("Forced Rebooting", fore="red")
         except Exception as e:
             clear()
-            print( f"{repr( e )} [rebooting]", fore="red" )
-            sleep( 2 )
-        sleep( 1 )
+            print(f"{repr(e)} [rebooting]", fore="red")
+            sleep(2)
+        sleep(1)
         clear()
-        os.chdir( root )
+        os.chdir(root)
--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 import setuptools
-from denver import pysetup
+from denverapi import pysetup
 
 with open("README.md") as file:
     long_description = file.read()
 
-requirements = ["denverapi",
+requirements = ["denver-api",
                 "setuptools~=50.3.0",
                 "requests",
                 "packaging"]
 
 setuptools.setup(
     name='PiOS',
-    version='0.3',
-    packages=setuptools.find_packages() + setuptools.find_namespace_packages(include=["pios.*"]),
+    version='0.4',
+    packages=setuptools.find_packages() + setuptools.find_namespace_packages(include=["pios.*", "pios"]),
     package_data=pysetup.find_package_data("pios", "pios"),
     url='https://github.com/xcodz-dot/PiOS',
     license='MIT License',
@@ -23,12 +23,5 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     python_requires=">=3.8",
     install_requires=requirements,
-    keywords=["PiOS", "python", "os", "github", "pyos", "operating", "system"]
-    entry_points={
-        "console_scripts": [
-            "pios-sdk-ppk = pios_sdk.ppk",
-            "pios-sdk-app = pios_sdk.app",
-            "pios-sdk-gensha256 = pios_sdk.generate_sha256"
-        ]
-    }
+    keywords=["PiOS", "python", "os", "github", "pyos", "operating", "system"],
 )
